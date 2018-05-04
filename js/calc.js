583b79ae1084fd69c77fbfde585c486d9ee186a3
sam-by-name <sam-sanders@hotmail.co.uk>
2018-05-04 12:35:18 +1200

    progress

---
+++ js/calc.js
@@ -0,0 +1,36 @@
+document.addEventListener('DOMContentLoaded', start)
+
+function start () {
+  bindEventListeners(document.getElementsByClassName('calc'), [0].children)
+}
+
+function numbers() {
+    
+}
+function clear() {
+
+}
+function delOne() {
+
+}
+function divide() {
+
+}
+function multiply() {
+
+}
+function minus() {
+
+}
+function plus() {
+
+}
+function equals() {
+
+}
+function percent() {
+
+}
+function decimal() {
+
+}
