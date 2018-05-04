42f7582c9a26056f06732d58e0c1171923750a8b
sam-by-name <sam-sanders@hotmail.co.uk>
2018-05-04 16:08:51 +1200

    well at least numbers function now

--- js/calc.js
+++ js/calc.js
@@ -1,17 +1,14 @@
-document.addEventListener('DOMContentLoaded', start)
+//document.addEventListener('DOMContentLoaded', start)
 
-function start () {
-  bindEventListeners(document.getElementsByClassName('calc'), [0].children)
-}
+//function start () {
+//  bindEventListeners(document.getElementsByClassName('calc'), [0].children)
+//}
 
-function numbers() {
-    
-}
-function clear() {
-
+function reset() {
+  document.getElementById("amount").value = '';
 }
 function delOne() {
-
+  document.getElementById("amount").value = 0;
 }
 function divide() {
 
@@ -34,3 +31,34 @@ function percent() {
 function decimal() {
 
 }
+function one () {
+  document.getElementById("amount").value += 1;
+}
+function two () {
+  document.getElementById("amount").value += 2;
+}
+function three () {
+  document.getElementById("amount").value += 3;
+}
+function four () {
+  document.getElementById("amount").value += 4;
+}
+function five () {
+  document.getElementById("amount").value += 5;
+}
+function six () {
+  document.getElementById("amount").value += 6;
+}
+function seven () {
+  document.getElementById("amount").value += 7;
+}
+function eight () {
+  document.getElementById("amount").value += 8;
+}
+function nine () {
+  document.getElementById("amount").value += 9;
+}
+function zero () {
+  document.getElementById("amount").value += 0;
+}
+
