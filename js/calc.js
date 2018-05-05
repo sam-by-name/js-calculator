07ab0ac56dac359d93cc021e22e4a565142a0718
sam-by-name <sam-sanders@hotmail.co.uk>
2018-05-05 20:47:21 +1200

    1 step forwards, two steps ..

--- js/calc.js
+++ js/calc.js
@@ -1,42 +1,62 @@
-//document.addEventListener('DOMContentLoaded', start)
+let tempNum;
+let current;
+//let tempTot;
 
-//function start () {
-//  bindEventListeners(document.getElementsByClassName('calc'), [0].children)
-//}
 
-function reset() {
-  document.getElementById("amount").value = ''; 
+////////////// CALCULATOR BUTTONS BELOW ////////////////////
+
+function reset() {                                         // AC button function
+  document.getElementById("amount").value = '';
+  tempNum = ''; 
 }
-function delOne() {
-  let current = document.getElementById("amount").value;  
-  let newCurrent = current.slice(0, -1);
-  document.getElementById("amount").value = newCurrent;
+
+function delOne() {                                        // CE button function
+  tempNum = tempNum.slice(0, -1);
+  document.getElementById("amount").value = tempNum;
+}
+
+function calcBtns (a) {                                    // num buttons function
+  tempNum = document.getElementById("amount").value += (a);
+}
+
+function plusBtn (a) {                                    // operator buttons function
+  tempNum = document.getElementById('amount').value += ' ' + a + ' ';
+}
+function minusBtn (a) {                                    // operator buttons function
+  tempNum = document.getElementById('amount').value += ' ' + a + ' ';
+}
+function timesBtn (a) {                                    // operator buttons function
+  tempNum = document.getElementById('amount').value += ' ' + a + ' ';
+}
+function divideBtn (a) {                                    // operator buttons function
+  tempNum = document.getElementById('amount').value += ' ' + a + ' ';
+}
+function decimalBtn (a) {                                    // operator buttons function
+  tempNum = document.getElementById('amount').value += a;
+}
+function percentageBtn (a) {                                    // operator buttons function
+  tempNum = document.getElementById('amount').value += a + ' ';
 }
 
 //////////////// Equals ///////////////////////////
 
-function equals() {
-  let current = [];
-  current = tempNum; // store all calc input to current
-  let mathArr = current.split('');       // mathArr turns current into a split array
-  let arrVar =[];
-  for (let i = 0; i < mathArr.length; i++) {
-    if (mathArr[i] == 1||2||3||4||5||6||7||8||9||0){
-    arrVar += mathArr[i];
+function equals() { 
+  let mathArr = tempNum.split(' ');
+  let joined = mathArr.join(' ');       // mathArr turns current into a split array
+  let pt1 = mathArr[0]+=mathArr[1]+=mathArr[2];
+  let pt2 = mathArr[3]+=mathArr[4];
+  
 
-  }}
-  alert(current);
+//  for (let i = 0; i < mathArr.length; i++) {
+//   // if (mathArr[i] ==  ){
+//    arrVar += mathArr[i];
+//
+//  }
+
+  alert(joined);
+
+
+
   //document.getElementById("amount").value = answer;
 }
 
-////////////// CALCULATOR BUTTONS BELOW ////////////////////
-
-function calcBtns (a) {
-  tempNum = document.getElementById("amount").value += a;
-}
-
-function opBtns (a) {
-  tempOp = document.getElementById('amount').value += a;
-}
-
-let tempNum;
