29ee14518e2e84cde79fa2098c4438f2fe44d9ef
sam-by-name <sam-sanders@hotmail.co.uk>
2018-05-08 11:31:48 +1200

    CE  -1 to display but not to variables yet

--- js/calc02.js
+++ js/calc02.js
@@ -3,11 +3,11 @@ let tempOp = '';
 let z = [];
 let ans = 0;  
 let y = [];
-var x = document.getElementById('amount').value; //doesn't work
+//let x = document.getElementById('amount').value; //doesn't work
 
 ////////////// CALCULATOR BUTTONS BELOW ////////////////////
 
-function reset() {                                         // AC button function
+function reset() {                                   // AC button function
   document.getElementById("amount").value = '';
   tempNum = '';
   tempOp = '';
@@ -16,41 +16,41 @@ function reset() {                                         // AC button function
   y = [];
 }
 
-function delOne() {                                        // CE button function
-  tempDisp = getElementById('amount').value;
-  newDisp = tempDisp.split('');
-  newDisp.slice(0, -1);
-  document.getElementById('amount').value = tempDisp;     // is now broke //////////
+function delOne() {                                  // CE button function
+  tempDisp = document.getElementById('amount').value;
+  newDisp = tempDisp.slice(0, -1);
+  document.getElementById('amount').value = newDisp;
+ 
 }
 
 //////////////////////////////////////
 
-function numBtns (a) {                                    // num buttons function
+function numBtns (a) {                               // num buttons function
   tempNum += a; 
   document.getElementById('amount').value += (a);
 }   
 
-function minusBtn (a) {                 // operator buttons function
-  if (tempNum == '') {                  // if tempNum is empty 
-    (tempNum = a) && (document.getElementById('amount').value += a);  // tempNum now holds '-'
+function minusBtn (a) {                              // operator buttons function
+  if (tempNum == '') {                               // if tempNum is empty 
+    ((tempNum = a) && (document.getElementById('amount').value += a));  // tempNum now holds '-'
   } else { opBtns ('-');
   }     
 }
 
-function opBtns (a) {                   // operator buttons function
+function opBtns (a) {                                // operator buttons function
   tempOp = a;
-  z.push(tempNum);                      // tempNum is pushed to sumArr
-  z.push(tempOp);                       // tempOp is pushed to sumArr
-  tempNum = '';                         // tempOp reset
-  tempOp = '';                          // tempNum reset
+  z.push(tempNum);                                   // tempNum is pushed to sumArr
+  z.push(tempOp);                                    // tempOp is pushed to sumArr
+  tempNum = '';                                      // tempOp reset
+  tempOp = '';                                       // tempNum reset
   document.getElementById('amount').value += a;
 }
 
-function decimalBtn (a) {                             // operator buttons function
+function decimalBtn (a) {                            // operator buttons function
   tempNum += '.';
   document.getElementById('amount').value += a;
 }
-function percentageBtn (a) {                          // operator buttons function
+function percentageBtn (a) {                         // operator buttons function
   tempNum += '%';
   document.getElementById('amount').value += a + ' ';
 }
@@ -58,16 +58,15 @@ function percentageBtn (a) {                          // operator buttons functi
 //////////////// Equals ///////////////////////////
 
 function equals() {
-  z.push(tempNum);    // make tempNum push to sumArr outside of here
-  
-  
+  z.push(tempNum);                                   // make tempNum push to sumArr outside of here
+
   // operators below
   let z1 = z[1];  
   let z2 = z[3];
   let z3 = z[5];
   let z4 = z[7];
-  let z5 = z[9];  
-                        // find a way to reset these z's and a's
+  let z5 = z[9];                                    // find a way to reset these z's and a's
+                                                    
   y  = z.map(Number);
   // numbers below
   let a1 = y[0];
@@ -80,24 +79,23 @@ function equals() {
   if        (z1 == '+') {ans = a1 + a2;} 
     else if (z1 == '*') {ans = a1 * a2;} 
     else if (z1 == '-') {ans = a1 - a2;} 
-    else if (z1 == '/') {ans = a1 / a2;}  
+    else if (z1 == '÷') {ans = a1 / a2;}  
   if        (z2 == '+') {ans = ans + a3;} 
     else if (z2 == '*') {ans = ans * a3;} 
     else if (z2 == '-') {ans = ans - a3;} 
-    else if (z2 == '/') {ans = ans / a3;}
+    else if (z2 == '÷') {ans = ans / a3;}
   if        (z3 == '+') {ans = ans + a4;} 
     else if (z3 == '*') {ans = ans * a4;} 
     else if (z3 == '-') {ans = ans - a4;} 
-    else if (z3 == '/') {ans = ans / a4;}
+    else if (z3 == '÷') {ans = ans / a4;}
   if        (z4 == '+') {ans = ans + a5;} 
     else if (z4 == '*') {ans = ans * a5;} 
     else if (z4 == '-') {ans = ans - a5;} 
-    else if (z4 == '/') {ans = ans / a5;}  
+    else if (z4 == '÷') {ans = ans / a5;}  
   if        (z5 == '+') {ans = ans + a6;} 
     else if (z5 == '*') {ans = ans * a6;} 
     else if (z5 == '-') {ans = ans - a6;} 
-    else if (z5 == '/') {ans = ans / a6;}
-  alert(ans);
+    else if (z5 == '÷') {ans = ans / a6;}
 document.getElementById("amount").value = ans;
 }
 
